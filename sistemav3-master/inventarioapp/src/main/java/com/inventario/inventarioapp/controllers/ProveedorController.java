ae915034716bebfe7a6e15efc858b047c25cd9d4
antonioamaya1 <62198833+antonioamaya1@users.noreply.github.com>
2020-03-20 00:08:11 -0600

    Update ProveedorController.java

--- sistemav3-master/inventarioapp/src/main/java/com/inventario/inventarioapp/controllers/ProveedorController.java
+++ sistemav3-master/inventarioapp/src/main/java/com/inventario/inventarioapp/controllers/ProveedorController.java
@@ -20,37 +20,69 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/proveedores")
 public class ProveedorController {
-    
+   
     @Autowired
+
     private ProveedorServiceImpl proveedorService;
 
+
+
     @RequestMapping(value="/", method = RequestMethod.GET)
+
     public List<Proveedor> getList(){
+
         List<Proveedor> proveedores = proveedorService.findAll();
+
         return proveedores;
+
     }
+
+
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
+
     public Optional<Proveedor> getcliente(@PathVariable("id") Long id){
+
         Optional<Proveedor> producto = proveedorService.findById(id);
+
         return producto;
+
     }
+
+
 
     @RequestMapping(value="/", method = RequestMethod.POST)
+
     public Proveedor guardar(@RequestBody Proveedor cliente){
+
         Proveedor cd = proveedorService.save(cliente);
+
         return cd;
+
     }
+
+
 
     @RequestMapping(value="/", method = RequestMethod.PUT)
+
     public Proveedor actualizar(@RequestBody Proveedor cliente){
+
         Proveedor cd = proveedorService.save(cliente);
+
         return cd;
+
     }
 
+
+
     @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+
     public void eliminar(@PathVariable("id") Long id){
+
         proveedorService.delete(id);
+
     }
+
+
 
 }
